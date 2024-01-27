15c6720be3d385b7a23fc61b111f46211e6226b0
stefanB <stefan_boghean@yahoo.com>
2024-01-27 23:00:13 +0200

    edited button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,6 @@
+import Popover from "@mui/material/Popover";
+
+import { useId, useState } from "react";
 import "./Button.scss";
 
 interface ButtonProps {
@@ -7,14 +10,49 @@ interface ButtonProps {
 }
 
 const Button = ({ name, title, link: url }: ButtonProps) => {
+  const [popoverAnchor, setPopoverAnchor] = useState<HTMLAnchorElement | null>(
+    null
+  );
+  const popoverId = useId();
+  const isEmail = name === "email";
+
+  const emailHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (!isEmail) return;
+    e.preventDefault();
+    setPopoverAnchor(e.currentTarget);
+    console.log("show popover");
+  };
+
+  const showPopover = Boolean(popoverAnchor);
+
+  const handlePopoverClose = () => {
+    setPopoverAnchor(null);
+  };
+
   return (
-    <a
-      className={`social-link social-link--${name}`}
-      href={url ? url : ""}
-      target="_blank"
-    >
-      {title}
-    </a>
+    <>
+      <a
+        className={`social-link social-link--${name}`}
+        href={url ? url : ""}
+        target="_blank"
+        onClick={emailHandler}
+      >
+        {title}
+      </a>
+      <Popover
+        className="save-email-popover"
+        id={popoverId}
+        open={showPopover}
+        anchorEl={popoverAnchor}
+        onClose={handlePopoverClose}
+        anchorOrigin={{
+          vertical: -40,
+          horizontal: 60,
+        }}
+      >
+        Email Address Copied!
+      </Popover>
+    </>
   );
 };
 
